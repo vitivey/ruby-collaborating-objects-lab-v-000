bded3ff524dd304f29faf104bb3a322830837636
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-17 02:33:19 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,14 +9,15 @@ class Song
 
     def self.new_by_filename(file) #file from MP3Importer class
       song_i = Song.new(file.split("-")[1].strip)  #create song instance using song string
-      artist_name=(file.split("-")[0].strip) #run artist_name method using artist string
+      artist_string=(file.split("-")[0].strip) #run artist_name method using artist string
       song_i
     end
 
-    def artist_name=(artist_name)
-      self.artist=Artist.find_or_create_by_name(artist_name) #finds / creates artist instance AND adds artist instance to song instance
-      self.artist.add_song(self) #adds song instance to artist instance
+    def artist_name=(artist_string)
+      self.artist=Artist.find_or_create_by_name(artist_string) #finds / creates artist instance AND adds artist instance to song instance
       binding.pry
+
+      self.artist.add_song(self) #adds song instance to artist instance
     end
 
 
